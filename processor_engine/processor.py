852c4f531d26f233e27cbd29905faee98fb81077
barsanmaity <barsanmaity7@gmail.com>
2026-07-20 16:10:21 +0000

    store fingerprints into json file

--- processor_engine/processor.py
+++ processor_engine/processor.py
@@ -1,5 +1,6 @@
 import librosa
 import numpy as np
+import json 
 
 def extract_fingerprint(audio_path):
     # Load the audio file (standardizing the sample rate to 22050 Hz)
@@ -11,4 +12,11 @@ def extract_fingerprint(audio_path):
     # Average the features across time to get a single, fixed-size 1D array
     fingerprint = np.mean(mfccs.T, axis=0)
     
-    return fingerprint
+    #decimal to integer 
+    scaledFingerPrint = [int(x * 10000) for x in fingerprint]
+    
+    #store to json 
+    with open('fingerprint.json', 'w') as f:
+        json.dump(scaledFingerPrint, f)
+    print("finger print save")
+    return scaledFingerPrint
