18bc13f8b41a35c3c42e93c19d3d55525b358e3c
barsanmaity <barsanmaity7@gmail.com>
2026-07-20 16:40:49 +0000

    injecting json, generate proof, verify proof

--- main_engine/main.go
+++ main_engine/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
 )
-
 
 //zk-circuit for the MFCC
 type Vcircuit struct {
@@ -28,8 +29,21 @@ func (circuit *Vcircuit) Define(api frontend.API) error {
 
 func main() {
 	fmt.Println("ZK Engine Initialized!")
-	var circuit Vcircuit
+    //json bridge
+	fmt.Println("reading json file..")
+	file, err:= os.Open("../processor_engine/fingerprint.json")
+	if err != nil {
+		log.Fatal("not file open: ", err)
+	}
+	defer file.Close()
 
+    var ectractedNumbers[13] int
+	if err := json.NewDecoder(file).Decode(&ectractedNumbers);
+	err != nil {
+		log.Fatal("could not parse json: ", err)
+	}
+
+	var circuit Vcircuit
 	//compile circuit to R1CS
     fmt.Println("compile the voice fingerprint")
 	compileConstraint, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
@@ -47,7 +61,36 @@ func main() {
 	 }
 	 fmt.Println("Proving key and Verifying key generated successfully")
 
-	//silence unused variables
-	_ = provingKey
-	_ = verifyingKey	
+	fmt.Println("injecting fingerprint..")
+	var witnessVars [13]frontend.Variable
+	for i := 0; i<13; i++ {
+          witnessVars[i] = ectractedNumbers[i]
+	}
+
+	assignment := Vcircuit{
+		Fingerprint: witnessVars,
+	}
+	witness, err := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
+	if err != nil {
+		log.Fatal("witness create error: ", err)
+	}
+
+	//generating proof 
+	fmt.Println("generating proof..")
+	proof, err := groth16.Prove(compileConstraint, provingKey, witness)
+	if err != nil {
+		log.Fatal("Prove error: ", err)
+	}
+	
+	//verify
+	publicWitness, err := frontend.NewWitness(&assignment, ecc.BN254.ScalarField(), frontend.PublicOnly())
+	if err != nil {
+		log.Fatal("public witness error: ", err)
+	}
+
+	err = groth16.Verify(proof, verifyingKey, publicWitness)
+	if err != nil {
+		log.Fatal("verification failed: ", err)
+	}
+	fmt.Println("verification success, proof complete ")
 }
